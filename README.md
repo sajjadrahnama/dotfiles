dbc9c7579b9f1524ed754347262a2ffb215db521
Sajjad Rahnama <sajjad.rahnama7@gmail.com>
2016-08-02 07:58:52 +0430

    Ubuntu install

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # dotfiles
 My Configuration
+
+## pre requesties
+
+### install oh-my-zsh
+	(http://ohmyz.sh)
+### install vundle for vim
+	(https://github.com/VundleVim/Vundle.vim)
---
+++ ubuntu_install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+cp ./env/aliases ~/.aliases
+cp ./env/exports ~/.exports
+cp ./zsh/sajjad.zsh-theme ~/.oh-my-zsh/themes
+cp ./zsh/zshrc ~/.zshrc
+cp -r ./vim/vim/colors ~/.vim
+cp ./vim/vimrc ~/.vimrc
