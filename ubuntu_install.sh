a3e60ae7733059d27bd68d7452fed99895a2470c
Sajjad Rahnama <sajjad.rahnama7@gmail.com>
2017-03-23 12:58:08 +0430

    socks command

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 cp ./env/aliases ~/.aliases
+cp ./env/mac_aliases ~/.mac_aliases
 cp ./env/exports ~/.exports
+cp -R ./scripts ~/.scripts
 cp ./zsh/sajjad.zsh-theme ~/.oh-my-zsh/themes
 cp ./zsh/zshrc ~/.zshrc
 cp -r ./vim/vim/colors ~/.vim
