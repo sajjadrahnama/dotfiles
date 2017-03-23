d3e5b7939067e7afaac680dd917739a5c9f44aaa
Sajjad Rahnama <sajjad.rahnama7@gmail.com>
2017-03-23 15:40:22 +0430

    directory bug fixes

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-cp ./env/aliases ~/.aliases
-cp ./env/mac_aliases ~/.mac_aliases
-cp ./env/exports ~/.exports
-cp -R ./scripts ~/.scripts
-cp ./zsh/sajjad.zsh-theme ~/.oh-my-zsh/themes
-cp ./zsh/zshrc ~/.zshrc
-cp -r ./vim/vim/colors ~/.vim
-cp ./vim/vimrc ~/.vimrc
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cp "$dir/env/aliases" ~/.aliases
+cp "$dir/env/mac_aliases" ~/.mac_aliases
+cp "$dir/env/exports" ~/.exports
+cp -R "$dir/scripts" ~/.scripts
+cp "$dir/zsh/sajjad.zsh-theme" ~/.oh-my-zsh/themes
+cp "$dir/zsh/zshrc" ~/.zshrc
+cp -r "$dir/vim/vim/colors" ~/.vim
+cp "$dir/vim/vimrc" ~/.vimrc
 mkdir -p ~/.vim/tmp
 mkdir -p ~/.vim/backup
