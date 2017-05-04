1bd4bb232f496ee26b47a646cd97c1cf0e45360d
Sajjad Rahnama <sajjad.rahnama7@gmail.com>
2017-05-04 11:42:26 +0430

    Brew Sbin added

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -4,6 +4,7 @@ dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cp "$dir/env/aliases" ~/.aliases
 cp "$dir/env/mac_aliases" ~/.mac_aliases
 cp "$dir/env/exports" ~/.exports
+cp "$dir/env/mac_exports" ~/.mac_exports
 cp -R "$dir/scripts/." ~/.scripts
 cp "$dir/zsh/sajjad.zsh-theme" ~/.oh-my-zsh/themes
 cp "$dir/zsh/zshrc" ~/.zshrc
